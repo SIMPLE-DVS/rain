266e063cc87a13cf296f718400c84d3ea04b2a55
Marco <marco.scarpetta@studenti.unicam.it>
2021-08-05 16:34:30 +0200

    Added SparkModelLoader, SparkSplitDataset, FilterRow

--- simple_repo/simple_spark/load/loader.py
+++ simple_repo/simple_spark/load/loader.py
@@ -1,4 +1,6 @@
-from simple_repo.simple_spark.spark_node import SparkParameter, Transformer
+from pyspark.ml import PipelineModel
+
+from simple_repo.simple_spark.spark_node import SparkParameter, Transformer, SparkNode
 
 
 class SparkCSVLoader(Transformer):
@@ -14,3 +16,20 @@ class SparkCSVLoader(Transformer):
 
     def execute(self):
         self.dataset = self.spark.read.csv(**self._get_attr_as_dict())
+
+
+class SparkModelLoader(SparkNode):
+
+    _attr = {
+        "path": SparkParameter("path", str, is_required=True)
+    }
+
+    _output = {
+        "model": PipelineModel
+    }
+
+    def __init__(self, spark, **kwargs):
+        super(SparkCSVLoader, self).__init__(spark, **kwargs)
+
+    def execute(self):
+        self.model = PipelineModel.load(self._attr.get("path").value)
--- simple_repo/simple_spark/transform/data_wrangling.py
+++ simple_repo/simple_spark/transform/data_wrangling.py
@@ -1,4 +1,6 @@
-from simple_repo.simple_spark.spark_node import Transformer, SparkParameterList
+from pyspark.sql import DataFrame
+
+from simple_repo.simple_spark.spark_node import Transformer, SparkParameterList, SparkParameter
 
 
 class SparkColumnSelector(Transformer):
@@ -12,3 +14,30 @@ class SparkColumnSelector(Transformer):
     def execute(self):
         columns = [c["col"] for c in self._attr.get("features").parameters]
         self.dataset = self.dataset.select(columns)
+        conditions = [c["value"] for c in self._attr.get("features").parameters if c["value"]]
+        for c in conditions:
+            self.dataset = self.dataset.filter(c)
+        self.dataset.show()
+
+
+class SparkSplitDataset(Transformer):
+    _attr = {
+        "train": SparkParameter("train", float, is_required=True),
+        "test": SparkParameter("test", float, is_required=True)
+    }
+
+    _output = {
+        "train_dataset": DataFrame,
+        "test_dataset": DataFrame
+    }
+
+    def __init__(self, spark, **kwargs):
+        super(SparkSplitDataset, self).__init__(spark, **kwargs)
+
+    def execute(self):
+        values = list(self._get_attr_as_dict().values())
+        self.train_dataset, self.test_dataset = self.dataset.randomSplit(values)
+        self.train_dataset.show()
+        self.test_dataset.show()
+
+
