d052449636602461f237be0559a268a0a8bd74bf
DazeDC <riccardo.coltrinari@studenti.unicam.it>
2021-08-24 11:43:08 +0200

    Added SplitTrainTest function skeleton.

---
+++ simple_repo/simple_sklearn/functions.py
@@ -0,0 +1,6 @@
+from node_structure import SklearnFunction
+
+
+class SimpleSplitTrainTest(SklearnFunction):
+    def __init__(self, **kwargs):
+        super(SimpleSplitTrainTest, self).__init__(**kwargs)
