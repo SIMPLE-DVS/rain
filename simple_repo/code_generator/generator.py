ad3cced9f17ff9f61a66b262d51c57ca2ef1aa77
DazeDC <riccardo.coltrinari@studenti.unicam.it>
2021-09-11 13:17:16 +0200

    Implemented new version of generator that now correctly prints all the classes and their dependencies.

--- simple_repo/code_generator/generator.py
+++ simple_repo/code_generator/generator.py
@@ -1,23 +1,11 @@
-import inspect
 from builtins import set
-from dataclasses import dataclass
-from queue import SimpleQueue, LifoQueue
-import types
-import re
-from jinja2 import FileSystemLoader, Template, Environment
+from queue import SimpleQueue
+from jinja2 import FileSystemLoader, Environment
 from enum import Enum
 import networkx as nx
 from matplotlib import pyplot as plt
 
-from simple_repo.code_generator.base import ObjectInfo
-from simple_repo.code_generator.class_analyzer import get_code
-from simple_repo.code_generator.exceptions_analyzer import extract_exceptions
-from simple_repo.code_generator.imports_analyzer import extract_imports
-from simple_repo.code_generator.parameters_analyzer import extract_parameters
-from simple_repo.code_generator.parents_analyzer import extract_parents
-from simple_repo.simple_pandas.load_nodes import PandasCSVLoader
-from simple_repo.simple_sklearn import svm
-from simple_repo.simple_pandas import load_nodes, transform_nodes
+from simple_repo.code_generator.class_analyzer import get_code, get_class_dependencies
 
 
 def get_obj_info(obj_set, obj_name):
@@ -36,76 +24,73 @@ class InfoType(Enum):
     NODE = "nodes"
 
 
-if __name__ == "__main__":
-
-    def show_dag(dag):
-        plt.tight_layout()
-        nx.draw_networkx(dag, arrows=True)
-        plt.show()
-        plt.clf()
-
-    nodes = set()
-    queue = SimpleQueue()
+def generate_code(classes: list):
     graph = nx.DiGraph()
-    queue.put(ObjectInfo(PandasCSVLoader, InfoType.NODE))
-    obj_infos = set()
-
     imports = set()
+    callables = set()
+
+    queue = SimpleQueue()
+
+    for cls in classes:
+        queue.put(cls)
 
     while not queue.empty():
-        edges = []
-        new_nodes = set()
+        cls = queue.get()
 
-        info = queue.get()
-        cls = info.obj_class
-        clsname = cls.__name__
+        cls_imports, cls_callables = get_class_dependencies(cls)
 
-        # add imports
-        [imports.add(imp) for imp in extract_imports(cls)]
+        difference = cls_callables.difference(callables)
 
-        new_nodes.update(
-            [ObjectInfo(exc, InfoType.EXCEPTION) for exc in extract_exceptions(cls)]
-        )
-        new_nodes.update(
-            [ObjectInfo(param, InfoType.PARAMETER) for param in extract_parameters(cls)]
-        )
-        new_nodes.update(
-            [ObjectInfo(par, InfoType.PARENT) for par in extract_parents(cls)]
-        )
+        for call in difference:
+            queue.put(call)
 
-        [
-            queue.put(new_obj)
-            for new_obj in list(filter(lambda x: x not in nodes, new_nodes))
-        ]
-
-        edges.extend([(node.obj_clsname, clsname) for node in new_nodes])
+        edges = [(call.__name__, cls.__name__) for call in cls_callables]
 
         graph.add_edges_from(edges)
 
-        nodes.update(new_nodes)
-        nodes.add(info)
+        imports.update(cls_imports)
+        callables.update(cls_callables)
+        callables.add(cls)
 
-        # show_dag(graph)
+    return imports, {cal.__name__: cal for cal in callables}, graph
 
-    [node.retrieve_code() for node in nodes]
 
+def code_generator(callables: dict, ordered_cal_names: list):
+    for cal_name in ordered_cal_names:
+        yield get_code(callables.get(cal_name))
+
+
+def write_file(imp, cal, graph):
     print_order = list(nx.topological_sort(graph))
 
     print(print_order)
 
-    nds = []
-
-    for node_name in print_order:
-        nds.append(get_obj_info(nodes, node_name))
-
     temp_file = FileSystemLoader("./")
+    # line_statement_prefix="#",
+    temp = Environment(loader=temp_file).get_template(name="pipeline_template.py.jinja")
 
-    temp = Environment(line_statement_prefix="#", loader=temp_file).get_template(
-        name="pipeline_template.py.jinja"
-    )
-
-    jinja_vars = {"imports": imports, "nodes": [node.code for node in nds]}
+    jinja_vars = {"imports": imp, "nodes": code_generator(cal, print_order)}
 
     # print(temp.render(parameters=parameters(), nodes_parents=[get_code(par) for par in pars]))
 
     temp.stream(**jinja_vars).dump("./pipeline.py")
+
+
+def show_dag(dag):
+    plt.tight_layout()
+    nx.draw_networkx(dag, arrows=True)
+    plt.show()
+    plt.clf()
+
+
+if __name__ == "__main__":
+    from simple_repo.simple_pandas.load_nodes import PandasCSVLoader
+    from simple_repo.simple_pandas.transform_nodes import PandasColumnSelector
+
+    imp, cal, graph = generate_code([PandasCSVLoader, PandasColumnSelector])
+
+    # show_dag(graph)
+
+    write_file(imp, cal, graph)
+
+    print("ok")
