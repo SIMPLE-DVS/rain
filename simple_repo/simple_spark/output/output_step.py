e457984a94a55c6ffa947bafe0f926eb5ed3c467
Marco <marco.scarpetta@studenti.unicam.it>
2021-10-06 16:13:41 +0200

    Spark refactor: output_step.py, spark_pipeline.py, stages.py

--- simple_repo/simple_spark/output/output_step.py
+++ simple_repo/simple_spark/output/output_step.py
@@ -1,32 +1,50 @@
 from pyspark.ml import PipelineModel
 from pyspark.sql import DataFrame
 
-from simple_repo.parameter import KeyValueParameter
+from simple_repo.parameter import KeyValueParameter, Parameters
 from simple_repo.simple_spark.node_structure import SparkNode
 
 
 class SaveModel(SparkNode):
+    """ Save a trained PipelineModel
+
+    Parameters
+    ----------
+    path: str
+        String representing the path where to save the model
+
+    """
+
     _input_vars = {"model": PipelineModel}
 
-    _parameters = {"path": KeyValueParameter("path", str, is_mandatory=True)}
-
-    def __init__(self, spark, **kwargs):
+    def __init__(self, spark, path: str):
+        self.parameters = Parameters(path=KeyValueParameter("path", str, path))
         super(SaveModel, self).__init__(spark)
 
     def execute(self):
-        self.model.write().overwrite().save(**self._get_params_as_dict())
+        self.model.write().overwrite().save(**self.parameters.get_dict())
 
 
 class SaveDataset(SparkNode):
+    """ Save a Spark Dataframe in a .csv format
+
+    Parameters
+    ----------
+    path: str
+        String representing the path where to save the dataset
+
+    index: bool = True
+        String representing the path where to save the dataset
+    """
+
     _input_vars = {"dataset": DataFrame}
 
-    _attr = {
-        "path": KeyValueParameter("path_or_buf", str, is_mandatory=True),
-        "index": KeyValueParameter("index", bool),
-    }
-
-    def __init__(self, spark, **kwargs):
+    def __init__(self, spark, path: str, index: bool = True):
+        self.parameters = Parameters(
+            path=KeyValueParameter("path_or_buf", str, path),
+            index=KeyValueParameter("index", bool, index)
+        )
         super(SaveDataset, self).__init__(spark)
 
     def execute(self):
-        self.dataset.toPandas().to_csv(**self._get_params_as_dict())
+        self.dataset.toPandas().to_csv(**self.parameters.get_dict())
--- simple_repo/simple_spark/pipeline/spark_pipeline.py
+++ simple_repo/simple_spark/pipeline/spark_pipeline.py
@@ -1,13 +1,24 @@
+from typing import List
+
 from pyspark.ml import Pipeline
 
-from simple_repo.simple_spark.node_structure import Estimator
+from simple_repo.simple_spark.node_structure import Estimator, SparkNode
 
 
 class SparkPipelineNode(Estimator):
+    """ Represent a Spark Pipeline consisting of SparkNode (stages)
+
+    Parameters
+    ----------
+    stages: str
+        List of SparkNode that can be executed in a Spark Pipeline
+
+    """
+
     _stages = []
 
-    def __init__(self, spark, lst, **kwargs):
-        for stage in lst:
+    def __init__(self, spark, stages: List[SparkNode]):
+        for stage in stages:
             self._stages.append(stage)
         super(SparkPipelineNode, self).__init__(spark)
 
