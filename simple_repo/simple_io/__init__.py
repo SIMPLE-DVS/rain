b1e19d714f0f0784411b49c7a7a04b2029fb6aa8
DazeDC <riccardo.coltrinari@studenti.unicam.it>
2021-10-15 17:25:35 +0200

    Added node to load iris dataset.

--- simple_repo/simple_io/__init__.py
+++ simple_repo/simple_io/__init__.py
@@ -1,3 +1,7 @@
-from simple_repo.simple_io.pandas_io import PandasCSVWriter, PandasCSVLoader
+from simple_repo.simple_io.pandas_io import (
+    PandasCSVWriter,
+    PandasCSVLoader,
+    PandasIrisLoader,
+)
 from simple_repo.simple_io.spark_input import SparkCSVLoader, SparkModelLoader
 from simple_repo.simple_io.spark_output import SaveModel, SaveDataset
