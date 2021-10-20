7333da8844f027a2c7da19e97c7d8bc59154cd85
DazeDC <riccardo.coltrinari@studenti.unicam.it>
2021-10-20 17:43:30 +0200

    Fixed bug in which TrainTestSampleTargetSplit worked with variables dataset and target whereas they declare sample_dataset and target_dataset.

--- simple_repo/simple_sklearn/__init__.py
+++ simple_repo/simple_sklearn/__init__.py
@@ -1,2 +1,6 @@
 from simple_repo.simple_sklearn.cluster import SimpleKMeans
 from simple_repo.simple_sklearn.svm import SklearnLinearSVC
+from simple_repo.simple_sklearn.functions import (
+    TrainTestSampleTargetSplit,
+    TrainTestDatasetSplit,
+)
