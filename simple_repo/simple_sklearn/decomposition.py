739acf5d31014e6f4e0775eddd275220636db913
DazeDC <riccardo.coltrinari@studenti.unicam.it>
2021-11-05 11:55:31 +0100

    Documentation Refactor.

--- simple_repo/simple_sklearn/decomposition.py
+++ simple_repo/simple_sklearn/decomposition.py
@@ -10,7 +10,26 @@ from simple_repo.simple_sklearn.node_structure import (
 
 class SklearnPCA(SklearnEstimator, TransformerMixin, ScorerMixin):
     """
+    Node representation of a sklearn PCA estimator.
+
+    Parameters
+    ----------
+    execute : list[str]
+        List of methods to execute.
+    n_components : int
+        Number of components to keep.
+    whiten : bool
+        When True (False by default) the components_ vectors are multiplied by the square root of n_samples and then divided by the singular values to ensure uncorrelated outputs with unit component-wise variances.
     svd_solver : {'auto', 'full', 'arpack', 'randomized'}, default='auto'
+        Svd solver.
+    tol : float
+        Tolerance for singular values computed by svd_solver == ‘arpack’.
+        Must be positive.
+    iterated_power : int
+        Number of iterations for the power method computed by svd_solver == ‘randomized’.
+        Must be positive.
+    random_state : int
+        Used when the ‘arpack’ or ‘randomized’ solvers are used. Pass an int for reproducible results across multiple function calls.
     """
 
     def __init__(
